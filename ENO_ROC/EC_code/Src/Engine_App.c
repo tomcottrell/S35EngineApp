3aa1c2b1b04857db26d6f34010767a1db8af69e5
tomcottrell <156876629+tomcottrell@users.noreply.github.com>
2025-10-08 14:00:06 +1100

    V0.0003

--- ENO_ROC/EC_code/Src/Engine_App.c
+++ ENO_ROC/EC_code/Src/Engine_App.c
@@ -7,6 +7,9 @@
 #include <stdint.h>
 #include <string.h>
 
+uint16_t Crank_Disconnect = 700;
+uint32_t slush_start_time = 0;
+
 engine_state_t engine_state = STOPPED;
 engine_action_t engine_action = NO_ACTION;
 
@@ -15,24 +18,23 @@ void engine_app()
 {
 	switch(engine_state){
 	case STOPPED:
-	Stopped();
+		Stopped();
 	break;
 	case STANDBY:
 
 	break;
 	case CRANK:
-
+		Crank();
 	break;
 	case CRANK_REST:
 
 	break;
 	case RUNNING:
-	Running();
+		Running();
 	break;
 	case SPINDOWN:
-
+		Spindown();
 	break;
-
 	// Add the default case if no option is set
 	}
 	//add stop checks function
@@ -42,9 +44,10 @@ void engine_app()
 // ============================================================================
 void Stopped()
 {
-	if((frequency > 600) && (frequency != 0xFFFF))
+	if((frequency != 0) && (frequency != 0xFFFF))
 	{
-		engine_state = RUNNING;
+		engine_state = CRANK;
+		slush_start_time = HAL_GetTick();
 	}
 }
 // ============================================================================
@@ -53,17 +56,59 @@ void Stopped()
 void Crank()
 {
 
+	if((frequency < Crank_Disconnect) || (frequency == 0xFFFF))
+	{
+		slush_start_time = HAL_GetTick();
+	if(frequency < 1 || (frequency == 0xFFFF))
+		{
+		engine_state = SPINDOWN;
+		}
+	}
+	else
+	{
+		if((HAL_GetTick() - slush_start_time) >= 1000)  // 1s elapsed
+		{
+			engine_state = RUNNING;
+		    slush_start_time = HAL_GetTick();  // Restart
+		}
+		//slush updates automatically
+	}
+
 }
 // ============================================================================
 // Engine Running State Function
 // ============================================================================
 void Running()
 {
-	if((frequency < 600) || (frequency == 0xFFFF))
+	if((frequency > Crank_Disconnect) || (frequency == 0xFFFF))
 	{
-		engine_state = STOPPED;
+		slush_start_time = HAL_GetTick();
+	}
+	else
+	{
+		if((HAL_GetTick() - slush_start_time) >= 2000)  // 2s elapsed
+		{
+			engine_state = SPINDOWN;
+		    slush_start_time = HAL_GetTick();  // Restart
+		}
 	}
 }
 // ============================================================================
-// Engine Stopped State Function
+// Engine Spindown State Function
 // ============================================================================
+void Spindown()
+{
+	if(frequency > 0 || (frequency != 0xFFFF))
+	{
+		slush_start_time = HAL_GetTick();
+	}
+	else
+	{
+		if((HAL_GetTick() - slush_start_time) >= 3000)  // 3s elapsed
+		{
+			engine_state = STOPPED;
+		    slush_start_time = HAL_GetTick();  // Restart
+		}
+	}
+}
+
